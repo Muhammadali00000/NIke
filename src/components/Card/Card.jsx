be763227b5dfbbd0e045fa8bcfd29afd34dbfa21
Muhammadali00000 <muhammadalinarov@gmail.com>
2024-10-19 16:44:32 +0500

    Add files via upload

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Card.css'
+const Card = () => {
+  return (
+    <>
+              <div className="card card1">
+            <img src="https://i.ibb.co/cNWqxGx/red.png" alt="" />
+            <h2>Nike X OFF-white</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <h3>$999</h3>
+
+            <div>
+              <button>Buy Now</button>
+              <button>⭐</button>
+            </div>
+          </div>
+    </>
+    )
+}
+
+export default Card
